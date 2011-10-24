f4f6b40e9aa0f05ec6031cfba04054ca0bf7b35d
auxtkw <auxtkw@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-24 23:40:32 +0000

    initial upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/phonegap/trunk@53325 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/ViewHelper/compass.php
@@ -0,0 +1,119 @@
+<?php
+
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011 Tim Wentzlau <tim.wentzlau@auxilior.com>, Auxilior Technology PF
+*
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/* This view helper implements Phonegap compass class.
+ * 
+ *
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ */
+class Tx_Phonegap_ViewHelpers_CompassViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper implements Tx_Fluid_Core_ViewHelper_Facets_ChildNodeAccessInterface {
+
+	public function __construct() {
+		$this->registerArgument('frequency', 'mixed', 'Value to be returned if the condition if met.', FALSE);
+		$this->registerArgument('success', 'mixed', 'Value to be returned if the condition if met.', FALSE);
+		$this->registerArgument('error', 'mixed', 'Value to be returned if the condition if not met.', FALSE);
+	}
+	
+	/**
+	 * An array of Tx_Fluid_Core_Parser_SyntaxTree_AbstractNode
+	 * @var array
+	 */
+	private $childNodes = array();
+
+	/**
+	 * Setter for ChildNodes - as defined in ChildNodeAccessInterface
+	 *
+	 * @param array $childNodes Child nodes of this syntax tree node
+	 * @return void
+	 */
+	public function setChildNodes(array $childNodes) {
+		$this->childNodes = $childNodes;
+	}
+	
+	
+	/**
+	 * renders <f:then> child if $condition is true, otherwise renders <f:else> child.
+	 *
+	 * 
+	 * @return string the rendered string
+	 */
+	public function render() {
+		$file=t3lib_extMgm::extPath('phonegap').'Public/phonegap.js';
+		$code='<script type="text/javascript" src="' . $file . '"></script>';
+		
+		//$GLOBALS['TSFE']->additionalHeaderData[$key] = $code;
+		//$GLOBALS['TSFE']->additionalHeaderData[] = '<meta name="mobile-access" content="geolocation">';
+		
+		$output='<script>
+				if (navigator.compass){
+					navigator.compass.watchHeading(
+						function (heading){'.$this->renderSuccessChild().';},
+						function (){'.$this->renderErrorChild().';},
+						{ frequency: '.$this->arguments["frequency"].' }
+					);
+				}
+			</script>
+			
+		';	
+		return $output;
+	}
+	
+	protected function renderSuccessChild() {
+		if ($this->arguments->hasArgument('success')) {
+			return $this->arguments['success'];
+		}
+
+		foreach ($this->childNodes as $childNode) {
+
+			
+			if ($childNode instanceof Tx_Fluid_Core_Parser_SyntaxTree_ViewHelperNode
+				&& $childNode->getViewHelperClassName() === 'Tx_Phonegap_ViewHelpers_SuccessViewHelper') {
+				$data = $childNode->evaluate($this->getRenderingContext());
+				return $data;
+			}
+		}
+		
+	}
+	
+	protected function renderErrorChild() {
+		if ($this->arguments->hasArgument('error')) {
+			return $this->arguments['error'];
+		}
+
+		foreach ($this->childNodes as $childNode) {
+
+			
+			if ($childNode instanceof Tx_Fluid_Core_Parser_SyntaxTree_ViewHelperNode
+				&& $childNode->getViewHelperClassName() === 'Tx_Phonegap_ViewHelpers_ErrorViewHelper') {
+				$data = $childNode->evaluate($this->getRenderingContext());
+				return $data;
+			}
+		}
+		
+	}
+	
+}
+?>
---
+++ ext_autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+$extensionClassesPath = t3lib_extMgm::extPath('phonegap', 'Classes/');
+$extensionPath = t3lib_extMgm::extPath('phonegap');
+$extbaseClassesPath = t3lib_extMgm::extPath('extbase', 'Classes/');
+return array(
+	'tx_phonegap_viewhelpers_cameraviewhelper' => $extensionClassesPath . '/ViewHelper/camera.php',
+	'tx_phonegap_viewhelpers_compassviewhelper' =>$extensionClassesPath . '/ViewHelper/compass.php',
+	'tx_phonegap_viewhelpers_successviewhelper' => $extensionClassesPath . '/ViewHelper/success.php',
+	'tx_phonegap_viewhelpers_errorviewhelper' => $extensionClassesPath . '/ViewHelper/error.php',
+
+);
